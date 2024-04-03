f7eedd609b567bf9d6bb67edfe9d031301ebe3e4
Daviti Dvali <dvali.daviti@gmail.com>
2024-04-03 16:56:01 +0400

    New Version

--- System.Extensions/ExtsDates.cs
+++ System.Extensions/ExtsDates.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace System.Exts
+﻿namespace System.Exts
 {
     public static partial class Extensions
     {
